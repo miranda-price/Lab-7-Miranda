fb3bb4befa15330cbfb4d886cdd145a02209aa6f
Miranda Price <miranda27price@gmail.com>
2021-07-30 21:57:00 -0500

    fixed index.php links

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 			<div class="card">
 				<section>
-					<h3><a href="ridgewood.html">Ridgewood, New Jersey</a></h3>
+					<h3><a href="ridgewood.php">Ridgewood, New Jersey</a></h3>
 					
 					<p>Ridgewood is a village in Bergen County, New Jersey, United States. As of the 2010 United States Census, the village population was 24,958, reflecting an increase of 22 from the 2000 Census, which had in turn increased by 784 from the 24,152  in the 1990 Census.Ridgewood is a suburban bedroom community of New York City, located approximately 20 miles northwest of Midtown Manhattan.</p>
 				</section>
@@ -12,7 +12,7 @@
 
 			<div class="card">
 				<section>
-				<h3><a href="chicago.html">Chicago, Illinois</a></h3>
+				<h3><a href="chicago.php">Chicago, Illinois</a></h3>
 				
 				<p>See what the Windy City has to offer! The City of Chicago covers an area of 60,000 hectares and sits 176 meters above sea level on the southwestern shore of Lake Michigan. At 190 km wide and 495 km long, Lake Michigan is the 5th largest body of fresh water in the world. Chicago is recognized across the United States as a very passionate sports town with teams in 5 major league sports.</p>
 				</section>
@@ -20,7 +20,7 @@
 
 			<div class="card">
 				<section>
-					<h3><a href="phoenix.html">Phoenix, Arizona</a></h3>
+					<h3><a href="phoenix.php">Phoenix, Arizona</a></h3>
 					
 					<p>Despite what you may have heard, Pheonix isn't that awesome of a city. There's barely any water! Who wants to live in a city where there's always a drought? It's hot. Like, all the time. So, if you love sweating through your clothes, come on down! Last but NOT least, in Phoenix the cacti aren't just a pretty picture anymore. We have jumping cacti that live up to their name and really attack you.</p>
 				</section>
